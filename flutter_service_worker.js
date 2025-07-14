050742e38b24409fc33aaaba183f1f9b00d216d9
SamiraAhmadnezhad <134923629+SamiraAhmadnezhad@users.noreply.github.com>
2025-07-14 18:03:02 +0000

    Deploying to gh-pages from @ SamiraAhmadnezhad/BookIt@02b5cfbd74e88123e5b09b4bcf4a50625d89cd57 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -272,7 +272,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "39af3d81993e3a3a7700777501640f8a
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "version.json": "9c1892e7e4290fd1c6e5226fa37e9bd3",
 "manifest.json": "a557db73668aae832682ecbd61e48423",
-"flutter_bootstrap.js": "1928b26744fe249427c2dfa83978b655",
+"flutter_bootstrap.js": "12d827d24a0b5cfdfc9422f0e5f1018e",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
